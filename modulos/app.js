9b922e7d42e0e80bfd17c676ca69868f0a1b3a80
João francisco <jfranciscobratti@gmail.com>
2023-04-24 16:00:04 -0300

    pronto

--- modulos/app.js
+++ modulos/app.js
@@ -1,3 +1,5 @@
+
 const meuModulo = require('./meuModulo');
 
 meuModulo.minhaFuncao(); // imprime "Olá do meu módulo!" no console
+
--- modulos/demo.js
+++ modulos/demo.js
@@ -1,6 +1,17 @@
 var http = require('http');
+<<<<<<< HEAD
+var url = require('url');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  var q = url.parse(req.url, true).query;
+  var txt = q.year + " " + q.month;
+  res.end(txt);
+}).listen(8080);
+=======
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write(req.url);
   res.end();
 }).listen(8080);
+>>>>>>> 80cd3a5b0dfc4ba6810641924869d9f4dd2f1cb9
