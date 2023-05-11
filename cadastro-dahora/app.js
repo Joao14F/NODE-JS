28e7b753bc8b64fad089bf33b386cd0e928c542e
João Francisco Bratti <jfranciscobratti@gmail.com>
2023-05-10 22:13:35 -0300

    pasta correta

---
+++ cadastro-dahora/app.js
@@ -0,0 +1,164 @@
+const express = require("express");
+const mysql = require("mysql");
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const connection = mysql.createConnection({
+  host: "127.0.0.1",
+  user: "root",
+  password: "aluno01",
+  database: "meuBanco"
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/cadastro", (req, res) => {
+  res.sendFile(__dirname + "/cadastro.html");
+});
+
+app.post("/cadastro", (req, res) => {
+  const { nome, endereco, altura, idade } = req.body;
+  if (!nome || !endereco || !altura || !idade) {
+    res.status(400).send("Nome, endereço, altura e idade são campos obrigatórios.");
+    return;
+  }
+
+  const cliente = { nome, endereco, altura, idade  };
+  connection.query("INSERT INTO clientes SET ?", cliente, (err, result) => {
+    if (err) throw err;
+    console.log(`Cliente ${nome} cadastrado com sucesso!`);
+    res.redirect("/");
+  });
+});
+
+// Rota para processar a listagem
+app.get('/listagem', (req, res) => {
+
+  // Consulta no banco de dados
+  connection.query(`SELECT * FROM clientes`, (error, results, fields) => {
+    if (error) throw error;
+    
+    // Exibição dos resultados
+    let html = `
+      <!DOCTYPE html>
+      <html>
+        <head>
+          <title>Clientes</title>
+        </head>
+        <body>
+          <h1>Clientes encontrados</h1>
+          <table>
+            <tr>
+              <th>Nome</th>
+              <th>endereço</th>
+              <th>altura</th>
+              <th>idade</th>
+            </tr>
+    `;
+    
+    results.forEach((cliente) => {
+      html += `
+        <tr>
+          <td>${cliente.nome}</td>
+          <td>${cliente.endereco}</td>
+          <td>${cliente.altura}</td>
+          <td>${cliente.idade}</td>
+        </tr>
+      `;
+    });
+    
+    html += `
+          </table>
+          <a href="/">Voltar</a>
+        </body>
+      </html>
+    `;
+    
+    res.send(html);
+  });
+});
+
+// Rota para exibir o formulário de consulta
+app.get('/consulta', (req, res) => {
+  res.send(`
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>Consulta de clientes</title>
+      </head>
+      <body>
+        <h1>Consulta de clientes</h1>
+        <form method="POST" action="/consulta">
+          <label for="nome">Nome:</label>
+          <input type="text" id="nome" name="nome"><br><br>
+          <button type="submit">Consultar</button>
+        </form>
+      </body>
+    </html>
+  `);
+});
+
+// Rota para processar a consulta
+app.post('/consulta', (req, res) => {
+  //const nome = req.body.nome;
+  const { nome, endereco, altura, idade } = req.body;
+  //const endereco = req.body.endereco;
+  //const endereco = req.body.altura;
+  //const endereco = req.body.idade;
+  
+  // Consulta no banco de dados
+  connection.query(`SELECT * FROM clientes WHERE nome LIKE '%${nome}%'`, (error, results, fields) => {
+    if (error) throw error;
+    
+    // Exibição dos resultados
+    let html = `
+      <!DOCTYPE html>
+      <html>
+        <head>
+          <title>Clientes</title>
+        </head>
+        <body>
+          <h1>Clientes encontrados</h1>
+          <table>
+            <tr>
+              <th>Nome</th>
+              <th>endereço</th>
+              <th>altura</th>
+              <th>idade</th>
+            </tr>
+    `;
+    
+    results.forEach((cliente) => {
+      html += `
+        <tr>
+          <td>${cliente.nome}</td>
+          <td>${cliente.endereco}</td>
+          <td>${cliente.altura}</td>
+          <td>${cliente.idade}</td>
+        </tr>
+      `;
+    });
+    
+    html += `
+          </table>
+          <a href="/">Voltar</a>
+        </body>
+      </html>
+    `;
+    
+    res.send(html);
+  });
+});
+
+connection.connect((err) => {
+  if (err) throw err;
+  console.log("Conectado ao banco de dados MySQL!");
+});
+
+app.listen(3000, () => {
+  console.log("Servidor iniciado na porta 3000");
+});
